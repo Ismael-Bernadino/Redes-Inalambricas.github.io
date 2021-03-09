9cf7abc89ec9f3038291c07d648b1cd6ce9ce868
Ismael-Bernadino <bernardinoabdalan@gmail.com>
2021-03-09 15:43:41 -0600

    v1

---
+++ js/nav.js
@@ -0,0 +1,9 @@
+var elementotop = $('.nav').offset().top;
+
+$(window).scroll(function () {
+    if( $(window).scrollTop() >= elementotop){
+        $('body').addClass('nav-fix');
+    }else{
+        $('body').removeClass('nav-fix');
+    }
+});
